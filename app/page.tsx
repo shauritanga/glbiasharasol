e16c0f00c9605d173f187fd9bc16f6b4f66bf943
Athanas Shauritanga <curtisisaac36@gmail.com>
2024-09-27 18:07:46 +0400

    apostr

--- app/page.tsx
+++ app/page.tsx
@@ -290,7 +290,7 @@ export default function Home() {
         <div className="bg-gray-100 py-20">
           <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
             <h2 className="text-3xl font-extrabold text-gray-900 mb-8">
-              People's overview on GLbiasharasol
+              People&apos;s overview on GLbiasharasol
             </h2>
             <div className="relative">
               <div
